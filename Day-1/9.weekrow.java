a5654443b19836d1b4fd1a6c986a362c5f1c05c9
SyedAmrez <112709867+SyedAmrez@users.noreply.github.com>
2022-10-13 13:07:08 +0530

    Update 9.weekrow.java

--- Day-1/9.weekrow.java
+++ Day-1/9.weekrow.java
@@ -1,24 +1,47 @@
-class weekrow {
-    public int[] kWeakestRows(int[][] M, int K) {
-        int y = M.length, x = M[0].length;
-        PriorityQueue<Integer> heap = new PriorityQueue<Integer>();
-        int[] ans = new int[K];
-        for (int i = 0; i < y; i++) {
-            heap.add(-(find(M[i]) << 7) - i);
-            if (heap.size() > K) heap.remove();
-        }
-        while (heap.size() > 0)
-            ans[heap.size()-1] = -heap.remove() & (1 << 7) - 1;
-        return ans;
-    }
-    int find(int[] row) {
-        int x = row.length;
-        int left = 0, right = x;
-        while (left <= right) {
-            int mid = left + right >> 1;
-            if (mid < x && row[mid] > 0) left = mid + 1;
-            else right = mid - 1;
-        }
-        return left;
-    }
+import java.util.*;
+public class weekrow
+{
+	public static void main(String[] args)
+	{
+		int i,j,n=5,c;
+		int a[][]=new int[n][n];
+		int x[]=new int[n];
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the matrix: ");
+		for(i=0;i<n;i++)
+		{
+			for(j=0;j<n;j++)
+			{
+				a[i][j]=s.nextInt();
+			}
+		}
+		for(i=0;i<n;i++)
+		{
+			c=0;
+			for(j=0;j<n;j++)
+			{
+				if(a[i][j]==1)
+				{
+					c++;
+				}	
+			}
+			System.out.println(i+" Row: "+c);
+			x[i]=c;
+		}
+		for(i=0;i<n;i++)
+		{
+			System.out.print(" "+x[i]);
+		}
+		int index = 0;
+    		int min = x[index];
+    		for (i = 1; i <n; i++)
+		{
+        		if (x[i] <= min)
+			{
+       			min = x[i];
+        			index = i;
+        		}
+    		}
+		System.out.print("\nWeakest row = "+index);
+	}
 }
